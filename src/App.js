39f3c4ec53ffbf6601b2e5fe41c4e4f70823cd48
wasiak93 <kacperjacekwasiak@gmail.com>
2020-05-11 18:41:57 +0200

    add handler filter cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 const App = () => {
   const [cardsArray, setCardsArray] = useState([]);
+  const [cardsArrayFilter, setCardsArrayFilter] = useState([]);
   const [number, setNumber] = useState(4);
   const [url, setUrl] = useState(
     `https://api.pokemontcg.io/v1/cards?pageSize=${number}`
@@ -40,10 +41,36 @@ const App = () => {
   };
 
   const handlerInput = (e) => {
-    setInputValue(e.target.value);
+    const value = e.target.value;
+    let newCards = cardsArray.filter((card) =>
+      card.name.toLowerCase().includes(value)
+    );
+    setCardsArrayFilter(newCards);
+    setInputValue(value);
   };
 
   const cards = cardsArray.map(
+    ({ id, name, imageUrl, supertype, subtype, rarity, number }) => (
+      <div key={id} className="card">
+        <p className="card__acapit card__acapit--flex">
+          <span className="card__span card__span--bigger">{name}</span>
+          Nr: {number}
+        </p>
+        <img src={imageUrl} alt="pokemon" className="card__img" />
+        <p className="card__acapit">
+          <span className="card__span">Superttype: </span>
+          {supertype}
+        </p>
+        <p className="card__acapit">
+          <span className="card__span">Subtype: </span> {subtype}
+        </p>
+        <p className="card__acapit">
+          <span className="card__span">Rarity: </span> {rarity}
+        </p>
+      </div>
+    )
+  );
+  const cardsFilter = cardsArrayFilter.map(
     ({ id, name, imageUrl, supertype, subtype, rarity, number }) => (
       <div key={id} className="card">
         <p className="card__acapit card__acapit--flex">
@@ -70,7 +97,7 @@ const App = () => {
         <h1 className="header__title">
           Pokemon <span className="header__span">catalog</span>
         </h1>
-        <div className="input--wrapper">
+        <div className="input__wrapper">
           <input
             type="text"
             value={inputValue}
@@ -78,11 +105,14 @@ const App = () => {
             onChange={handlerInput}
             placeholder="Search"
           />
-          <div className="input__line"></div>
+          <div className="header__line"></div>
+          <div className="header__input-subtitle">
+            {inputValue ? "Search" : null}
+          </div>
         </div>
       </div>
 
-      <div className="cards--wrapper">{cards}</div>
+      <div className="cards--wrapper">{inputValue ? cardsFilter : cards}</div>
       {isLoading ? <p className="loader">"loading"</p> : null}
       <button className="button" onClick={handlerButton}>
         Load More

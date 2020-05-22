0600bd2b205ef4f73c2cd372c606ef9075197a39
wasiak93 <kacperjacekwasiak@gmail.com>
2020-05-22 17:59:32 +0200

    remove trash code

--- src/components/Card/CardTypeAcapit.js
+++ src/components/Card/CardTypeAcapit.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const CardTypeAcapit = ({ bigger, name, type }) => {
-  // console.log(bigger == true);
   return (
     <p className="card__acapit">
       <span className={`card__type ${bigger ? "card__type--bigger" : ""}`}>
